4841b1edc64da69625f19e2d7557fa79a6c7408f
Andre Guedes <andre.guedes@trustly.com>
2025-07-10 17:40:39 -0300

    Connect my bank button event

--- app/src/main/java/net/trustly/trustlysdkdemoandroid/ui/MainActivity.kt
+++ app/src/main/java/net/trustly/trustlysdkdemoandroid/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
@@ -32,6 +33,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<AppCompatButton>(R.id.btnConnectMyBank).setOnClickListener {
+            openLightbox()
+        }
 
         if (EstablishData.DYNAMIC_REQUEST_SIGNATURE) postRequestSignature()
         else initWidget()
